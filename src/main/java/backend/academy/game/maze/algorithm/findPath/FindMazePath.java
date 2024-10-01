3ec72cc6831f345c4b6cd1b725aa255e497aa022
GG_Host <gd.host@yandex.ru>
2024-10-01 22:44:28 +0300

    feat: created methods to create maze and find the path of the maze

--- src/main/java/backend/academy/game/maze/algorithm/findPath/FindMazePath.java
+++ src/main/java/backend/academy/game/maze/algorithm/findPath/FindMazePath.java
@@ -1,4 +1,9 @@
 package backend.academy.game.maze.algorithm.findPath;
 
+import backend.academy.game.maze.cell.Cell;
+import it.unimi.dsi.fastutil.Pair;
+import java.util.List;
+
 public interface FindMazePath {
+    List<Pair<Integer, Integer>> find(List<List<Cell>> maze);
 }
--- src/main/java/backend/academy/game/maze/algorithm/generate/CreateMaze.java
+++ src/main/java/backend/academy/game/maze/algorithm/generate/CreateMaze.java
@@ -1,4 +1,8 @@
 package backend.academy.game.maze.algorithm.generate;
 
+import backend.academy.game.maze.cell.Cell;
+import java.util.List;
+
 public interface CreateMaze {
+    List<List<Cell>> createMaze();
 }
