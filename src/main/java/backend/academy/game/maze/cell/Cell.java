5119b6bd560470db85674dcd9c60d89dfc67cf29
GG_Host <gd.host@yandex.ru>
2024-10-08 16:42:50 +0300

    refactor: specified method equals and hashcode of custom class Cell

--- src/main/java/backend/academy/game/maze/cell/Cell.java
+++ src/main/java/backend/academy/game/maze/cell/Cell.java
@@ -1,5 +1,9 @@
 package backend.academy.game.maze.cell;
 
+import lombok.Getter;
+import java.util.Objects;
+
+@Getter
 public abstract class Cell {
     protected final int x;
     protected final int y;
@@ -7,5 +11,17 @@ public abstract class Cell {
     public Cell(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cell cell = (Cell) o;
+        return x == cell.x && y == cell.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
