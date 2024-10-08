f214145bd89d373fcd487e39750eb83ea3716d19
GG_Host <gd.host@yandex.ru>
2024-10-09 01:31:31 +0300

    feat: added some util functions

--- src/main/java/backend/academy/game/maze/algorithm/MazeUtils.java
+++ src/main/java/backend/academy/game/maze/algorithm/MazeUtils.java
@@ -4,13 +4,22 @@ import backend.academy.game.maze.cell.Cell;
 import java.util.List;
 
 public interface MazeUtils {
-    default int getX(int x) {
+    default int getRealX(int x) {
         return x*2+1;
     }
-    default int getY(int y) {
+    default int getRealY(int y) {
         return y*2+1;
     }
+    default int getX(int realX) {
+        return (realX-1)/2;
+    }
+    default int getY(int realY) {
+        return (realY-1)/2;
+    }
     default Cell getCell(int x, int y, List<List<Cell>> maze) {
-        return maze.get(getY(y)).get(getX(x));
+        return maze.get(getRealY(y)).get(getRealX(x));
+    }
+    default Cell getRealCell(int x, int y, List<List<Cell>> maze) {
+        return maze.get(y).get(x);
     }
 }
--- src/main/java/backend/academy/game/maze/algorithm/findPath/FindMazePath.java
+++ src/main/java/backend/academy/game/maze/algorithm/findPath/FindMazePath.java
@@ -1,6 +1,7 @@
 package backend.academy.game.maze.algorithm.findPath;
 
 import backend.academy.game.maze.algorithm.MazeUtils;
+import backend.academy.game.maze.algorithm.Point;
 import backend.academy.game.maze.cell.Cell;
 import it.unimi.dsi.fastutil.Pair;
 import java.util.List;
@@ -15,5 +16,16 @@ public interface FindMazePath extends MazeUtils {
             Pair.of(1, 0),
             Pair.of(-1, 0)
         );
+    }
+    default List<Pair<Integer, Integer>> realDirections() {
+        return List.of(
+            Pair.of(0, 2),
+            Pair.of(0, -2),
+            Pair.of(2, 0),
+            Pair.of(-2, 0)
+        );
+    }
+    default boolean isValidDestination(Point to, int width, int height) {
+        return to.x() > 0 && to.y() > 0 && to.x() < width-1 && to.y() < height-1;
     }
 }
