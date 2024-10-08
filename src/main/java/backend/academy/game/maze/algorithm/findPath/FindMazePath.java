e1eb42ce8c8594dabd46015a7b7abe351992873c
GG_Host <gd.host@yandex.ru>
2024-10-08 16:43:38 +0300

    feat: created a function to get all possible directions

--- src/main/java/backend/academy/game/maze/algorithm/findPath/FindMazePath.java
+++ src/main/java/backend/academy/game/maze/algorithm/findPath/FindMazePath.java
@@ -1,9 +1,19 @@
 package backend.academy.game.maze.algorithm.findPath;
 
+import backend.academy.game.maze.algorithm.MazeUtils;
 import backend.academy.game.maze.cell.Cell;
 import it.unimi.dsi.fastutil.Pair;
 import java.util.List;
 
-public interface FindMazePath {
-    List<Pair<Integer, Integer>> apply(List<List<Cell>> maze);
+public interface FindMazePath extends MazeUtils {
+    List<Pair<Integer, Integer>> apply(List<List<Cell>> maze, Pair<Integer, Integer> start, Pair<Integer, Integer> end);
+
+    default List<Pair<Integer, Integer>> directions() {
+        return List.of(
+            Pair.of(0, 1),
+            Pair.of(0, -1),
+            Pair.of(1, 0),
+            Pair.of(-1, 0)
+        );
+    }
 }
