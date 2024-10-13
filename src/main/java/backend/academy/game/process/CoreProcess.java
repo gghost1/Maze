2d32c9f6e094283f999ab23ecbdf200f78f270c2
GG_Host <gd.host@yandex.ru>
2024-10-14 02:11:15 +0300

    feat: fixed implementation of methods used maze

--- src/main/java/backend/academy/game/process/CoreProcess.java
+++ src/main/java/backend/academy/game/process/CoreProcess.java
@@ -1,13 +1,19 @@
 package backend.academy.game.process;
 
+import backend.academy.controller.MazeGenerationController;
+import backend.academy.controller.MazeSolvingController;
+import backend.academy.exception.NotInitializedException;
 import backend.academy.exception.PathNotFoundException;
 import backend.academy.game.maze.Maze;
+import backend.academy.game.process.maze.MazeGenerationProcess;
+import backend.academy.game.process.maze.MazeProcess;
+import backend.academy.game.process.maze.MazeSolvingProcess;
 import java.lang.reflect.InvocationTargetException;
 
 public class CoreProcess {
 
     private final SettingsProcess settingsProcess;
-    private Maze maze;
+    private MazeProcess maze;
 
     public CoreProcess(SettingsProcess settingsProcess)
         throws NoSuchMethodException,
@@ -16,21 +22,19 @@ public class CoreProcess {
             IllegalAccessException
     {
         this.settingsProcess = settingsProcess;
-        this.maze = new Maze(
+        this.maze = new MazeProcess(new Maze(
             settingsProcess.mazeWidth(),
             settingsProcess.mazeHeight(),
             settingsProcess.createMazeAlgorithm().getAlgorithm().getConstructor().newInstance(),
             settingsProcess.findMazePathAlgorithm().getAlgorithm().getConstructor().newInstance()
-        );
+        ), new MazeProcess.MazeSettings(settingsProcess.start(), settingsProcess.end()));
     }
 
-    public void createMaze() {
-        maze.generateMaze(settingsProcess.start(), settingsProcess.end());
+    public void createMaze() throws NotInitializedException {
+        new MazeGenerationController(new MazeGenerationProcess(maze)).execute();
     }
 
-    public void solveMaze() throws PathNotFoundException {
-        maze.solveMaze(settingsProcess.start(), settingsProcess.end());
-        // TODO add output in MazeController
+    public void solveMaze() throws NotInitializedException {
+        new MazeSolvingController(new MazeSolvingProcess(maze)).execute();
     }
-
 }
