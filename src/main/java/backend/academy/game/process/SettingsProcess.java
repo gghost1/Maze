ccf9c82f3c6d748fdc1517651a3c57d6ad9aeaf9
GG_Host <gd.host@yandex.ru>
2024-10-14 03:13:24 +0300

    feat: added the validation to setting process redundant methods removed

--- src/main/java/backend/academy/game/process/SettingsProcess.java
+++ src/main/java/backend/academy/game/process/SettingsProcess.java
@@ -1,28 +1,33 @@
 package backend.academy.game.process;
 
+import backend.academy.exception.IllegalSettingParameter;
 import backend.academy.game.maze.algorithm.Point;
 import backend.academy.game.maze.algorithm.findPath.FindMazePathAlgorithm;
 import backend.academy.game.maze.algorithm.generate.CreateMazeAlgorithm;
 import lombok.Getter;
 import lombok.Setter;
+import java.security.SecureRandom;
 import java.util.Optional;
 
 @Getter
+@Setter
 public class SettingsProcess {
-
-    @Setter private int mazeWidth;
-    @Setter private int mazeHeight;
-    @Setter private Point start;
-    @Setter private Point end;
+    private int mazeWidth;
+    private int mazeHeight;
+    private Point start;
+    private Point end;
     private CreateMazeAlgorithm createMazeAlgorithm;
     private FindMazePathAlgorithm findMazePathAlgorithm;
 
-    public void setCreateMazeAlgorithm(Optional<CreateMazeAlgorithm> createMazeAlgorithm) {
-        this.createMazeAlgorithm = createMazeAlgorithm.get();
-    }
-
-    public void setFindMazePathAlgorithm(Optional<FindMazePathAlgorithm> findMazePathAlgorithm) {
-        this.findMazePathAlgorithm = findMazePathAlgorithm.get();
+    public boolean isValid() {
+        if (mazeWidth <= 0 || mazeHeight <= 0) {
+            throw new IllegalSettingParameter("Maze size must be greater than zero");
+        } else if (start.x() < 0 || start.y() < 0 || end.x() < 0 || end.y() < 0) {
+            throw new IllegalSettingParameter("Start and end must be greater than zero");
+        } else if (start.x() > mazeWidth || start.y() > mazeHeight || end.x() > mazeWidth || end.y() > mazeHeight) {
+            throw new IllegalSettingParameter("Start and end must be less than maze size");
+        }
+        return true;
     }
 
 }
