eef04f0258ba033b537cc1a1735a4b5696a5ea2b
GG_Host <gd.host@yandex.ru>
2024-10-14 04:01:02 +0300

    feat: added dictionary for executable classes

--- src/main/java/backend/academy/controller/Executable.java
+++ src/main/java/backend/academy/controller/Executable.java
@@ -3,12 +3,16 @@ package backend.academy.controller;
 import backend.academy.controller.listener.Listener;
 import backend.academy.exception.NotInitializedException;
 import backend.academy.io.CustomOutput;
+import backend.academy.io.language.Dictionary;
+import backend.academy.io.language.LanguageManager;
 
 public abstract class Executable {
     protected final Listener listener;
     protected final CustomOutput output;
+    protected final Dictionary dictionary;
 
     protected Executable() throws NotInitializedException {
+        dictionary = LanguageManager.dictionary();
         listener = Listener.getInstance();
         output = CustomOutput.getInstance();
     }
