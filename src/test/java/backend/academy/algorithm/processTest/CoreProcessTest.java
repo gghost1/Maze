0ac4a622ab46804312ece66cebf75f8679379eb9
GG_Host <gd.host@yandex.ru>
2024-10-14 03:15:58 +0300

    feat: added tests for core and setting processes

---
+++ src/test/java/backend/academy/algorithm/processTest/CoreProcessTest.java
@@ -0,0 +1,70 @@
+package backend.academy.algorithm.processTest;
+
+import backend.academy.controller.listener.Listener;
+import backend.academy.exception.UnsuccessfulPreviousProcess;
+import backend.academy.game.maze.algorithm.Point;
+import backend.academy.game.maze.algorithm.findPath.DeadEndFiller;
+import backend.academy.game.maze.algorithm.findPath.FindMazePathAlgorithm;
+import backend.academy.game.maze.algorithm.generate.CreateMazeAlgorithm;
+import backend.academy.game.process.CoreProcess;
+import backend.academy.game.process.SettingsProcess;
+import backend.academy.io.CustomOutput;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.StandardCharsets;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CoreProcessTest {
+
+    @BeforeEach
+    public void setUp() {
+        Listener.getInstance(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+        CustomOutput.getInstance(new PrintWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8)));
+    }
+
+    @Test
+    public void createCoreProcessTest() {
+        SettingsProcess settingsProcess = getSettingsProcess();
+        assertDoesNotThrow(() -> new CoreProcess(settingsProcess));
+    }
+
+    @Test
+    public void createCoreProcessNegativeTest() {
+        SettingsProcess settingsProcess = getSettingsProcess();
+        settingsProcess.mazeWidth(0);
+        settingsProcess.mazeHeight(0);
+        assertThrows(UnsuccessfulPreviousProcess.class, () -> new CoreProcess(settingsProcess));
+    }
+
+    @Test
+    public void generateMazeTest() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        SettingsProcess settingsProcess = getSettingsProcess();
+        CoreProcess coreProcess = new CoreProcess(settingsProcess);
+        assertDoesNotThrow(coreProcess::createMaze);
+    }
+
+    @Test
+    public void solveMazeTest() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        SettingsProcess settingsProcess = getSettingsProcess();
+        CoreProcess coreProcess = new CoreProcess(settingsProcess);
+        assertDoesNotThrow(coreProcess::solveMaze);
+    }
+
+    private SettingsProcess getSettingsProcess() {
+        SettingsProcess settingsProcess = new SettingsProcess();
+        settingsProcess.mazeWidth(5);
+        settingsProcess.mazeHeight(5);
+        settingsProcess.start(new Point(0, 0));
+        settingsProcess.end(new Point(4, 4));
+        settingsProcess.createMazeAlgorithm(CreateMazeAlgorithm.PRIMS);
+        settingsProcess.findMazePathAlgorithm(FindMazePathAlgorithm.DEAD_END_FILLER);
+        return settingsProcess;
+    }
+
+}
---
+++ src/test/java/backend/academy/algorithm/processTest/SettingProcessTest.java
@@ -0,0 +1,64 @@
+package backend.academy.algorithm.processTest;
+
+import backend.academy.exception.IllegalSettingParameter;
+import backend.academy.game.maze.algorithm.Point;
+import backend.academy.game.maze.algorithm.findPath.FindMazePathAlgorithm;
+import backend.academy.game.maze.algorithm.generate.CreateMazeAlgorithm;
+import backend.academy.game.process.SettingsProcess;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class SettingProcessTest {
+
+    @Test
+    public void sizeValidationTest() {
+        SettingsProcess settingsProcess = getSettingsProcess();
+        settingsProcess.mazeWidth(0);
+        assertThrows(IllegalSettingParameter.class, settingsProcess::isValid);
+
+        settingsProcess = getSettingsProcess();
+        settingsProcess.mazeHeight(0);
+        assertThrows(IllegalSettingParameter.class, settingsProcess::isValid);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideIllegalPoints")
+    public void startPointValidationTest(Point point) {
+        SettingsProcess settingsProcess = getSettingsProcess();
+        settingsProcess.start(point);
+        assertThrows(IllegalSettingParameter.class, settingsProcess::isValid);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideIllegalPoints")
+    public void endPointValidationTest(Point point) {
+        SettingsProcess settingsProcess = getSettingsProcess();
+        settingsProcess.end(point);
+        assertThrows(IllegalSettingParameter.class, settingsProcess::isValid);
+    }
+
+    private static Stream<Point> provideIllegalPoints() {
+        return Stream.of(
+            new Point(-1, 0),
+            new Point(0, -1),
+            new Point(6, 0),
+            new Point(0, 6)
+        );
+    }
+
+    private SettingsProcess getSettingsProcess() {
+        SettingsProcess settingsProcess = new SettingsProcess();
+        settingsProcess.mazeWidth(5);
+        settingsProcess.mazeHeight(5);
+        settingsProcess.start(new Point(0, 0));
+        settingsProcess.end(new Point(4, 4));
+        settingsProcess.createMazeAlgorithm(CreateMazeAlgorithm.PRIMS);
+        settingsProcess.findMazePathAlgorithm(FindMazePathAlgorithm.DEAD_END_FILLER);
+        return settingsProcess;
+    }
+}
